8a1baf045910541428508a0eebb86f922624db82
Cristianprogramar <111073514+Cristianprogramar@users.noreply.github.com>
2023-05-03 06:09:32 -0300

    Mejoras en la estructura

--- src/main/java/com/portfolio/cristian/Controller/PersonaController.java
+++ src/main/java/com/portfolio/cristian/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //Permito el acceso a el front
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 
 public class PersonaController {
     
