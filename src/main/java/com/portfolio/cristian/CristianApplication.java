31adbd3d15a97d9d6f10f4e7fd9e18eec83ed5ac
Cristianprogramar <111073514+Cristianprogramar@users.noreply.github.com>
2023-05-03 23:13:47 -0300

    Estructuración del código

--- src/main/java/com/portfolio/cristian/CristianApplication.java
+++ src/main/java/com/portfolio/cristian/CristianApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CristianApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CristianApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CristianApplication.class, args);
+    }
 }
