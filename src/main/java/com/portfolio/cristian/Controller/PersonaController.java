0d9553308043dedb136dfad5611320e401813b7c
Cristianprogramar <111073514+Cristianprogramar@users.noreply.github.com>
2023-05-08 17:36:44 -0300

    Estructura del back terminada
    Se finalizó la estructura del back, ya funciona de manera adecuada.

--- src/main/java/com/portfolio/cristian/Controller/PersonaController.java
+++ src/main/java/com/portfolio/cristian/Controller/PersonaController.java
@@ -1,89 +1,86 @@
 package com.portfolio.cristian.Controller;
 
+import com.portfolio.cristian.Dto.dtoPersona;
 import com.portfolio.cristian.Entity.Persona;
-import com.portfolio.cristian.Interface.IPersonaService;
+import com.portfolio.cristian.Security.Controller.Mensaje;
+import com.portfolio.cristian.Service.ImpPersonaService;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 @CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired
-    IPersonaService ipersonaService;
+    ImpPersonaService personaService;
 
-    //Traer la lista de usuarios
-    @GetMapping("user/get")
-    public List<Persona> getPersona() {
-        List<Persona> personas = ipersonaService.getPersona();
-        if (personas.isEmpty()) {
-            throw new IllegalStateException("No se pudo obtener la lista.");
+    //Obtener una lista de los usuarios
+    @GetMapping("/list")
+    public ResponseEntity<List<Persona>> list() {
+        List<Persona> list = personaService.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+
+    //Obtener un usuario por ID
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Persona> getById(@PathVariable("id") int id) {
+        if (!personaService.existsById(id)) {
+            return new ResponseEntity(new Mensaje("No se encontró el usuario."), HttpStatus.NOT_FOUND);
         }
-        return personas;
+        Persona persona = personaService.getOne(id).get();
+        return new ResponseEntity(persona, HttpStatus.OK);
     }
 
-    //Crear la persona
-    @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping("/user/create")
-    public String createPersona(@RequestBody Persona persona) {
-        if (persona.getNombre() == null || persona.getNombre().isEmpty()
-                || persona.getApellido() == null || persona.getApellido().isEmpty()
-                || persona.getImg() == null || persona.getImg().isEmpty()) {
-            throw new IllegalArgumentException("Todos los campos son obligatorios.");
+    //Eliminar un usuario por ID
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        personaService.delete(id);
+        return new ResponseEntity(new Mensaje("El usuario fue eliminado."), HttpStatus.OK);
+    }
+
+    //Crear un nuevo usuario
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody dtoPersona dtopersona) {
+        if (StringUtils.isBlank(dtopersona.getNombre())) {
+            return new ResponseEntity(new Mensaje("Los campos son obligatorios."), HttpStatus.BAD_REQUEST);
         }
-        ipersonaService.savePersona(persona);
-        return "El usuario fue creado.";
+
+        Persona persona = new Persona(dtopersona.getNombre(), dtopersona.getApellido(), dtopersona.getDescription(), dtopersona.getImg());
+        personaService.save(persona);
+
+        return new ResponseEntity(new Mensaje("El usuario fue agregado."), HttpStatus.OK);
     }
 
-    //Eliminar la persona por ID
-    @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/user/delete/{id}")
-    public ResponseEntity<String> deletePersona(@PathVariable Long id) {
-        try {
-            ipersonaService.deletePersona(id);
-            return ResponseEntity.ok("El usuario fue eliminado.");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("No se pudo eliminar el usuario.");
+    //Actualizar un usuario por ID
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoPersona dtopersona) {
+        if (!personaService.existsById(id)) {
+            return new ResponseEntity(new Mensaje("No se encontró el usuario."), HttpStatus.BAD_REQUEST);
         }
-    }
+        if (StringUtils.isBlank(dtopersona.getNombre())) {
+            return new ResponseEntity(new Mensaje("Los campos son obligatorios."), HttpStatus.BAD_REQUEST);
+        }
 
-    //Editar la persona por ID
-    @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/user/edit/{id}")
-    public Persona editPersona(@PathVariable Long id,
-            @RequestParam("nombre") String nuevoNombre,
-            @RequestParam("apellido") String nuevoApellido,
-            @RequestParam("img") String nuevoImg) {
-        Persona persona = ipersonaService.findPersona(id);
+        Persona persona = personaService.getOne(id).get();
+        
+        persona.setNombre(dtopersona.getNombre());
+        persona.setApellido(dtopersona.getApellido());
+        persona.setDescription(dtopersona.getDescription());
+        persona.setImg(dtopersona.getImg());
 
-        persona.setNombre(nuevoNombre);
-        persona.setApellido(nuevoApellido);
-        persona.setImg(nuevoImg);
-
-        ipersonaService.savePersona(persona);
-        return persona;
-    }
-
-    //Traer mi nombre al front
-    @GetMapping("/user/get/profile")
-    public Persona findPersona() {
-        return ipersonaService.findPersona((long) 1);
-    }
-
-    //Manejar las excepciones
-    @ExceptionHandler(IllegalArgumentException.class)
-    public String handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ex.getMessage();
+        personaService.save(persona);
+        return new ResponseEntity(new Mensaje("El usuario fue actualizado."), HttpStatus.OK);
     }
 }
--- src/main/java/com/portfolio/cristian/Entity/Persona.java
+++ src/main/java/com/portfolio/cristian/Entity/Persona.java
@@ -7,15 +7,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter @Setter
 @Entity
 public class Persona implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
     
     @NotNull
     @Size(min = 1, max = 50, message = "Debe ingresar al menos un dato.")
@@ -25,6 +22,61 @@ public class Persona implements Serializable {
     @Size(min = 1, max = 50, message = "Debe ingresar al menos un dato.")
     private String apellido;
     
-    @Size(min = 1, max = 50, message = "Debe ingresar al menos un dato.")
+    @Size(min = 1, max = 500, message = "Debe ingresar al menos un dato.")
+    @NotNull
+    private String description;
+    
     private String img;
+    
+    //Constructores
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String description, String img) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.description = description;
+        this.img = img;
+    }
+    
+    //Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
 }
